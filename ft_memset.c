e942a3a7f6444712527d9c9a7e31a60ff98f15ca
KSNG <keagan.saw@gmail.com>
2025-05-08 18:00:40 +0800

    update

--- ft_memset.c
+++ ft_memset.c
@@ -1,11 +1,26 @@
 
 
-int main()
+#include "libft.h"
+#include <stdio.h>
+#include <stddef.h>
+
+
+void *ft_memset(void *str, int a, size_t n)
 {
-    size_t a;
-        
-    return 0;
+    while (n > 0)
+    {
+        ((unsigned char *)str)[n - 1] = (unsigned char)a;
+        n--;
+    }
+    return (str);
+}
 
-
-    
+int	main()
+{
+	char s[] = "123456";
+	char t[] = "123456";
+	memset(s, 'a', 3);
+	ft_memset(t, 'a', 3);
+	printf("OG string: %s", s);
+	printf("\nft string: %s", t);
 }
---
+++ libft.h
@@ -0,0 +1,10 @@
+
+#ifndef LIBFT_H
+
+#define LIBFT_H
+
+#include <stddef.h>
+#include <unistd.h>
+
+#endif
+
